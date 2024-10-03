e7038be72fbd2d4c009f28bfd5dea7cdc52f9242
Sameeranjoshi <joshisameeran17@gmail.com>
2024-10-03 02:05:47 -0600

    fix the algo, assembly dump fails

--- compiler_x86_64.cpp
+++ compiler_x86_64.cpp
@@ -14,14 +14,71 @@ class Compiler {
 private:
     std::vector<char> preprocessed;
     std::unordered_map<int, std::string> loop_labels;
+    std::unordered_map<int, std::unordered_map<int, int>> dollar_to_offsetcount;
     bool optimize_simple_loops;
     bool optimize_memory_scans;
+
+    bool is_simple_loop(size_t start, size_t end) { // [...] contains brackets
+        std::unordered_map<int, int> offset_count;
+        int pointer_movement = 0;
+        int net_change = 0;
+        for (size_t i = start + 1; i < end; ++i) {  // body of loop
+            char command = preprocessed[i];
+            switch (command) {
+                case '>': pointer_movement++; break;
+                case '<': pointer_movement--; break;
+                case '+': offset_count[pointer_movement]++; break;
+                case '-': offset_count[pointer_movement]--; break;
+                // if [+$] or even [+0] this is not a simple loop
+                case '[': case ']': case '.': case ',': case '$': case '0': return false; // Nested loops or I/O
+            }
+        }
+        return pointer_movement == 0 && (offset_count[0] == 1 || offset_count[0] == -1);
+    }
+
+    std::unordered_map<int, int> getoffset_count_map(size_t start, size_t end) {
+        std::unordered_map<int, int> offset_count;
+        // special case for single command loops    // [+] or [-]
+        // if (end - start == 2){
+        //     offset_count[0] = 0;
+        //     return offset_count;
+        // }
+        int pointer_movement = 0;
+        for (size_t i = start + 1; i < end; ++i) {
+            char command = preprocessed[i];
+            switch (command) {
+                case '>': pointer_movement++; break;
+                case '<': pointer_movement--; break;
+                case '+': offset_count[pointer_movement]++; break;
+                case '-': offset_count[pointer_movement]--; break;
+            }
+        }
+        return offset_count;
+    }
+
+    void rewrite_$_for_loop(size_t start, size_t end, std::unordered_map<int, int>& offset_count_map) {
+        // use the formula start + offset * offset_count to optimize the loop and dump assembly
+                                // print some debugging code to make sure everything is working
+        
+        // print start and end
+        // std::cout << "start " << start << "end "   << end << "\n";
+        // replace [++] -> $000
+        for (size_t i = start; i <= end; i++)
+        {
+            if (i == start){
+                preprocessed[i] = '$';
+            } else {
+                preprocessed[i] = '0';
+            }
+        }
+        dollar_to_offsetcount[start] = offset_count_map;
+    }
 
 public:
     Compiler(bool optimize_simple_loops, bool optimize_memory_scans)
         : optimize_simple_loops(optimize_simple_loops), optimize_memory_scans(optimize_memory_scans) {}
 
-    void preprocess_code() {
+    void assign_loop_label() {
         std::vector<int> stack;
         for (size_t idx = 0; idx < preprocessed.size(); ++idx) {
             char command = preprocessed[idx];
@@ -91,18 +148,9 @@ public:
         assembly_file << "\tcall memset                # Call memset function\n";
     }
 
-    void compile(const std::string& code, const std::string& filename, std::ofstream &assembly_file) {
-        for (char eachword : code) {
-            if (std::string("><+-.,[]").find(eachword) != std::string::npos) {
-                preprocessed.push_back(eachword);
-            }
-        }
-        preprocess_code();
-        
-        initial_setup_assembly_structure(assembly_file);
+    void gen_assembly(std::ofstream &assembly_file, const std::string& filename) {
 
-        size_t PC_index = 0;
-        while (PC_index < preprocessed.size()) {
+        for (int PC_index = 0; PC_index < preprocessed.size(); ++PC_index) {
             char instruction = preprocessed[PC_index];
 
             switch (instruction) {
@@ -138,17 +186,77 @@ public:
                     assembly_file << loop_labels[PC_index] + "_end" << ":\n";
                     assembly_file << "#LOOP END\n";
                     break;
+                case '$':
+                    assembly_file << "# Optimized loop\n";
+                    // using dollar_to_offsetcount write assembly to do maths
+                    for (auto& [offset, count] : dollar_to_offsetcount[PC_index]) {
+                        // skip the value with offset == 0
+                        if (offset == 0) {
+                            continue;
+                        }
+                        assembly_file << "\tmovq %r12, %rax\n";
+                        assembly_file << "\taddq $" << offset << ", %rax\n";        // rax = new index                    
+                        // multiply count with the value at the offset
+                        assembly_file << "\tmovb (%r12), %bl\n";
+                        // multiple value at bl with count
+                        assembly_file << "\timulb $" << count << ", %bl\n"; // bl = count*p[base]
+                        // add the value at the offset with the count
+                        assembly_file << "\taddb %bl, (%rax)\n"; // p[base+offset] = p[base+offset] + count*p[base]
+                    }
+                    // for offset=0 make // make p[offset] = 0
+                    assembly_file << "\tmovb $0, (%r12)\n";
+                    assembly_file << "# Optimized loop end\n";
+                    break;
+                case '0':
+                    // this is a special case when the loop is optimized into some commands using the 
+                    break;
                 default:
                     std::cerr << "Failed to compile code from file=" << filename
                               << ", at position = " << PC_index
                               << ": and at instruction = '" << instruction << "'" << std::endl;
                     exit(1);
             }
-
-            PC_index++;
         }
+    }
 
+    void optimize(std::ofstream &assembly_file, const std::string& filename) {
+        // implement simple_loop_optimization here.
+        if (optimize_simple_loops) {
+            std::vector<int> stack;
+            for (size_t idx = 0; idx < preprocessed.size(); ++idx) {
+                char command = preprocessed[idx];
+                if (command == '[') {
+                    stack.push_back(idx);
+                } else if (command == ']') {
+                    if (stack.empty()) {
+                        throw std::runtime_error("Unmatched ']' found");
+                    }
+                    int start = stack.back();
+                    stack.pop_back();
+                    if (is_simple_loop(start, idx)) {
+                        auto offset_count_map = getoffset_count_map(start, idx);
+                        rewrite_$_for_loop(start, idx, offset_count_map);
+                    }
+                }
+            }
+            if (!stack.empty()) {
+                throw std::runtime_error("Unmatched '[' found");
+            }
+        }
+    }
+
+    void compile(const std::string& code, const std::string& filename, std::ofstream &assembly_file) {
+        for (char eachword : code) {
+            if (std::string("><+-.,[]").find(eachword) != std::string::npos) {
+                preprocessed.push_back(eachword);
+            }
+        }
+        optimize(assembly_file, filename);
+        assign_loop_label();
+        initial_setup_assembly_structure(assembly_file);
+        gen_assembly(assembly_file, filename);
         final_setup_assembly_structute(assembly_file);
+
         std::cout << "\nSuccessfully compiled code from file=" << filename << std::endl;
     }
 };
