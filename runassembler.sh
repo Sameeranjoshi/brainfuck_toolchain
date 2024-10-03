1c1dc916e03f2eaa83fe7fc0ae675bb622974290
Sameeranjoshi <joshisameeran17@gmail.com>
2024-10-03 02:39:53 -0600

    final cleanup

--- runassembler.sh
+++ runassembler.sh
@@ -9,22 +9,22 @@ mkdir -p timing_results
 # Benchmarks in benches folder
 for file in benches/*.b; do
     # Run without --optimize-simple-loops
+    echo "Processing $file " >> timing_results/all_results_no_loop.time
     ./wrapper $file
     as -o program.o assembly_output.s 
     gcc -o out program.o -lc
     
     # Measure the time and save the result
-    echo "Results for $(basename $file) without --optimize-simple-loops:" >> timing_results/all_results_no_loop.time
     { time ./out; } 2>> timing_results/all_results_no_loop.time
     echo "" >> timing_results/all_results_no_loop.time
 
     # Run with --optimize-simple-loops
+    echo "Processing $file " >> timing_results/all_results_with_loop.time
     ./wrapper $file --optimize-simple-loops 
     as -o program.o assembly_output.s 
     gcc -o out program.o -lc
     
     # Measure the time and save the result
-    echo "Results for $(basename $file) with --optimize-simple-loops:" >> timing_results/all_results_with_loop.time
     { time ./out; } 2>> timing_results/all_results_with_loop.time
     echo "" >> timing_results/all_results_with_loop.time
 done
