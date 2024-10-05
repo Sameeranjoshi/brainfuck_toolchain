f26a18554c84df6be9de5d7fd5e05d4a3fd9f2be
Sameeranjoshi <joshisameeran17@gmail.com>
2024-10-04 21:45:46 -0600

    use function make generic

--- runassembler.sh
+++ runassembler.sh
@@ -7,47 +7,22 @@ clang++ -std=c++17 -O3 -o wrapper compiler_x86_64.cpp
 mkdir -p timing_results
 
 # Benchmarks in benches folder
+run_benchmark() {
+    local file=$1
+    local options=$2
+    local result_file=$3
+
+    echo "Processing $file " >> $result_file
+    ./wrapper $file $options
+    as -o program.o assembly_output.s
+    gcc -o out program.o -lc
+    { time ./out; } 2>> $result_file
+    echo "" >> $result_file
+}
+
 for file in benches/*.b; do
-    # Run with --baseline
-    echo "Processing $file " >> timing_results/all_results_baseline.time
-    ./wrapper $file
-    as -o program.o assembly_output.s 
-    gcc -o out program.o -lc
-    
-    # Measure the time and save the result
-    { time ./out; } 2>> timing_results/all_results_baseline.time
-    echo "" >> timing_results/all_results_baseline.time
-
-
-    # Run with --optimize-simple-loops
-    echo "Processing $file " >> timing_results/all_results_with_loop.time
-    ./wrapper $file --optimize-simple-loops 
-    as -o program.o assembly_output.s 
-    gcc -o out program.o -lc
-    
-    # Measure the time and save the result
-    { time ./out; } 2>> timing_results/all_results_with_loop.time
-    echo "" >> timing_results/all_results_with_loop.time
-
-    # Run with --optimize-memory-scans
-    echo "Processing $file " >> timing_results/all_results_with_scan.time
-    ./wrapper $file --optimize-memory-scans
-    as -o program.o assembly_output.s
-    gcc -o out program.o -lc
-
-    # Measure the time and save the result
-    { time ./out; } 2>> timing_results/all_results_with_scan.time
-    echo "" >> timing_results/all_results_with_scan.time
-
-
-    # Run with --optimize-simple-loops and --optimize-memory-scans
-    echo "Processing $file " >> timing_results/all_results_with_loop_and_scan.time
-    ./wrapper $file --optimize-simple-loops --optimize-memory-scans
-    as -o program.o assembly_output.s
-    gcc -o out program.o -lc
-    # Measure the time and save the result
-    { time ./out; } 2>> timing_results/all_results_with_loop_and_scan.time
-    echo "" >> timing_results/all_results_with_loop_and_scan.time
-
-
+    run_benchmark $file "" "timing_results/all_results_baseline.time"
+    run_benchmark $file "--optimize-simple-loops" "timing_results/all_results_with_loop.time"
+    run_benchmark $file "--optimize-memory-scans" "timing_results/all_results_with_scan.time"
+    run_benchmark $file "--optimize-simple-loops --optimize-memory-scans" "timing_results/all_results_with_loop_and_scan.time"
 done
